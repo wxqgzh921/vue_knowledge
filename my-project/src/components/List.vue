efa066edba8cd06e42867a4b4f5ae5100e2929e4
wxqgzh921 <963836594@qq.com>
2019-05-30 00:26:12 +0800

    update

--- my-project/src/components/List.vue
+++ my-project/src/components/List.vue
@@ -1,7 +1,7 @@
 <!-- List.vue 函数式组件 -->
 <template functional>
     <div>
-        <p v-for="item in props.items" @click="props.itemClick(item)">
+        <p v-for="(item,index) in props.items" @click="props.itemClick(item)" :key="index">
             {{ item }}
         </p>
     </div>
